fd15d2246f842be7360f7e28309416854dd7bd79
bibi2812 <114660896+bibi2812@users.noreply.github.com>
2023-03-20 22:07:05 +0000

    20/03/2023 - notes item made

--- lib/notesPage/widgets/viewBody.dart
+++ lib/notesPage/widgets/viewBody.dart
@@ -12,8 +12,48 @@ class viewNotesBody extends StatelessWidget {
         SizedBox(
           height: 50,
         ),
-        appBar()
+        appBar(),
+        itemNotes(),
       ],
+    );
+  }
+}
+
+class itemNotes extends StatelessWidget {
+  const itemNotes ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top:20,bottom: 20, left:16),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(16),
+    ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text("Date",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 26,
+            ),
+            ),
+            subtitle: const Text("Mood Score",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+            ),
+            ),
+            trailing: IconButton(onPressed: () {}, icon: const Icon(Icons.delete,
+            color: Colors.black,
+              size: 25,
+            ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
