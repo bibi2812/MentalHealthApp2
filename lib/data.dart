d9266b602619d74d96d9a59438d019a471534d92
bibi2812 <114660896+bibi2812@users.noreply.github.com>
2023-03-22 00:08:21 +0000

    22/03/2023 - started hive implementation

--- lib/data.dart
+++
@@ -1,8 +0,0 @@
-class DataBase{
-
-  int dateID;
-  int ratingID;
-  String entryID;
-
-  DataBase (this.dateID, this.ratingID, this.entryID);
-}
---
+++ lib/models/notesModel.dart
@@ -0,0 +1,11 @@
+class notesModel {
+  final String date;
+  final String moodScore;
+  final int color;
+
+  notesModel({
+    required this.date,
+    required this.moodScore,
+    required this.color
+  });
+}
